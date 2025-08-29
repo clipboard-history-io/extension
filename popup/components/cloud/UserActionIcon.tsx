197b35c1b966a383e55decf2492cfba535d700ef
ayoung19 <andyluyoung@gmail.com>
2025-08-29 12:31:44 -0700

    feat: indicator for mobile app

--- popup/components/cloud/UserActionIcon.tsx
+++ popup/components/cloud/UserActionIcon.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Menu, Text, Tooltip } from "@mantine/core";
+import { ActionIcon, Indicator, Menu, Text, Tooltip, useMantineTheme } from "@mantine/core";
 import {
   IconCreditCard,
   IconDeviceMobile,
@@ -6,12 +6,19 @@ import {
   IconUserCircle,
   IconWorld,
 } from "@tabler/icons-react";
+import { useAtomValue } from "jotai";
 import { useState } from "react";
 
+import { changelogViewedAtAtom } from "~popup/states/atoms";
+import { updateChangelogViewedAt } from "~storage/changelogViewedAt";
 import db from "~utils/db/react";
 import env from "~utils/env";
+import { lightOrDark } from "~utils/sx";
+import { VERSION } from "~utils/version";
 
 export const UserActionIcon = () => {
+  const theme = useMantineTheme();
+  const changelogViewedAt = useAtomValue(changelogViewedAtAtom);
   const auth = db.useAuth();
   const connectionStatus = db.useConnectionStatus();
   const [opened, setOpened] = useState(false);
@@ -27,9 +34,15 @@ export const UserActionIcon = () => {
           label={<Text fz="xs">{chrome.i18n.getMessage("commonProfile")}</Text>}
           disabled={opened}
         >
-          <ActionIcon variant="light" color="indigo.5">
-            <IconUserCircle size="1.125rem" />
-          </ActionIcon>
+          <Indicator
+            color={lightOrDark(theme, "red.4", "red.6")}
+            size={8}
+            disabled={changelogViewedAt === VERSION}
+          >
+            <ActionIcon variant="light" color="indigo.5" onClick={() => updateChangelogViewedAt()}>
+              <IconUserCircle size="1.125rem" />
+            </ActionIcon>
+          </Indicator>
         </Tooltip>
       </Menu.Target>
 
