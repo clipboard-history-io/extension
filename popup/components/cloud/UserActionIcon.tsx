d94558931ff9d774d5dbf7673bcf64ca94158994
Andy Young <andyluyoung@gmail.com>
2026-01-11 11:47:06 -1000

    Revert "feat: indicator for mobile app"
    This reverts commit 197b35c1b966a383e55decf2492cfba535d700ef.

--- popup/components/cloud/UserActionIcon.tsx
+++ popup/components/cloud/UserActionIcon.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Indicator, Menu, Text, Tooltip, useMantineTheme } from "@mantine/core";
+import { ActionIcon, Menu, Text, Tooltip } from "@mantine/core";
 import {
   IconCreditCard,
   IconDeviceMobile,
@@ -6,19 +6,12 @@ import {
   IconUserCircle,
   IconWorld,
 } from "@tabler/icons-react";
-import { useAtomValue } from "jotai";
 import { useState } from "react";
 
-import { changelogViewedAtAtom } from "~popup/states/atoms";
-import { updateChangelogViewedAt } from "~storage/changelogViewedAt";
 import db from "~utils/db/react";
 import env from "~utils/env";
-import { lightOrDark } from "~utils/sx";
-import { VERSION } from "~utils/version";
 
 export const UserActionIcon = () => {
-  const theme = useMantineTheme();
-  const changelogViewedAt = useAtomValue(changelogViewedAtAtom);
   const auth = db.useAuth();
   const connectionStatus = db.useConnectionStatus();
   const [opened, setOpened] = useState(false);
@@ -34,15 +27,9 @@ export const UserActionIcon = () => {
           label={<Text fz="xs">{chrome.i18n.getMessage("commonProfile")}</Text>}
           disabled={opened}
         >
-          <Indicator
-            color={lightOrDark(theme, "red.4", "red.6")}
-            size={8}
-            disabled={changelogViewedAt === VERSION}
-          >
-            <ActionIcon variant="light" color="indigo.5" onClick={() => updateChangelogViewedAt()}>
-              <IconUserCircle size="1.125rem" />
-            </ActionIcon>
-          </Indicator>
+          <ActionIcon variant="light" color="indigo.5">
+            <IconUserCircle size="1.125rem" />
+          </ActionIcon>
         </Tooltip>
       </Menu.Target>
 
